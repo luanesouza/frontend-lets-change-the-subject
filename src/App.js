fa4130ba1e03f14811a4da971327a9a720ca9e69
luanesouza <luanesouza411@gmail.com>
2020-03-28 17:48:30 -0400

    removes extra files, adds components

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { Route, withRouter } from 'react-router-dom'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Homepage from './components/Homepage';
+
+class App extends Component {
+
+  state = {
+    username: '',
+    password: '',
+    email: '',
+    cards: [],
+    friendsCards: [],
+    coworkersCards: [],
+    partnerCards: []
+  }
+
+  handleChange(event){
+    const { name, value } = event.target
+
+    this.setState({
+      [name]: value
+    });
+  }
+
+  async handleSubmit(evt){
+    evt.preventDefault()
+
+  }
+
+  render() {
+    console.log(this.props);
+    return (
+      <main className="App">
+        <Homepage />
+      </main>
+    );
+  }
 }
 
-export default App;
+export default withRouter(App);
---
+++ src/components/Homepage.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default function Homepage(){
+
+  return(
+    <section>
+      <h1> Let's Change the Subject </h1>
+      <p> Conversations made easy </p>
+
+      <button value='Sign Up'/>
+      <a> Already have an account? </a>
+      <a> Skip for Now </a>
+    </section>
+  )
+}
