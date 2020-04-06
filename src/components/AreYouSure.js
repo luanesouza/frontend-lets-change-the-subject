2f39b901b250d177a0db967e2047c2bdd9b15243
luanesouza <luanesouza411@gmail.com>
2020-04-06 17:23:15 -0400

    styling of are you sure page done

--- src/components/AreYouSure.js
+++ src/components/AreYouSure.js
@@ -6,15 +6,17 @@ export default function AreYouSure(props) {
 
   const confirmation = (evt) => {
     evt.preventDefault()
-    console.log(props.isUserLeaving(!props.leaving))
+    console.log(props.isUserLeaving(false))
   }
 
   return(
-    <section classsName='AreYouSure'>
-      <h4> Sure you'd like to leave this conversation?</h4>
-      <section className='sure-buttons'>
-        <button> YES </button>
-        <button onClick={(evt) => confirmation(evt)}> NO </button>
+    <section id='AreYouSure'>
+      <section className='confirmation-content'>
+        <h4> Sure you'd like to leave this conversation?</h4>
+        <section className='sure-buttons'>
+          <button> YES </button>
+          <button onClick={(evt) => confirmation(evt)}> NO </button>
+        </section>
       </section>
     </section>
   )
