53b5f4dc50b9995477654043b30238e13d5498fd
luanesouza <luanesouza411@gmail.com>
2020-05-03 11:52:10 -0400

    fixes grammar

--- src/components/Warning.js
+++ src/components/Warning.js
@@ -5,7 +5,7 @@ export default function Warning() {
     <section className='warning'>
       <div>
         <h1> Access from computer detected </h1>
-        <p> For a better experience, please use your phone to navigate this app. <br/> We promise you you won't regret it.</p>
+        <p> For a better experience, please use your phone to navigate this app. <br/> We promise you, you won't regret it.</p>
       </div>
     </section>
   )
